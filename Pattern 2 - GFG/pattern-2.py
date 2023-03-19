1592d815967c4615ce302d6175926d2411b1cf8b
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-03-19 15:51:27 +0530

    Added solution - LeetHub

---
+++ Pattern 2 - GFG/pattern-2.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        
+        
+        for i in range(N):
+            for j in range(i+1):
+                print('*', end = " ")
+            print()
+                
+        # Code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
