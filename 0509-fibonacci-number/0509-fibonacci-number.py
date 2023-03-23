d542006c9ce3fb1d2320ec0e4079ea64456f531e
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-03-23 12:46:09 +0530

    Time: 35 ms (57.95%), Space: 13.9 MB (42.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
         
+        #Iterative
+        a,b = 0,1
+        
+        for i in range(n):
+            a,b = b, a+b
+        
+        return a
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Recursive
         if n < 2:
             return n
         return self.fib(n-1) + self.fib(n-2)
