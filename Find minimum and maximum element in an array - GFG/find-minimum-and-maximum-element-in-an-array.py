0ce2c197694b3a7a1f2ab39b5a0596a50d490aea
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-04-16 22:43:25 +0530

    Added solution - LeetHub

---
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+def getMinMax( a, n):
+    
+    minEle = float("inf")
+    maxEle = float("-inf")
+    
+    
+    for i in a:
+        minEle = min(minEle,i)
+        maxEle = max(maxEle,i)
+    
+    return minEle,maxEle
+    
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = getMinMax(a, n)
+        print(product[0], end=" ")
+        print(product[1])
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends
