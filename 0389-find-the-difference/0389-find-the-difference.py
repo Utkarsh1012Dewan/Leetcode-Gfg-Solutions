1a6077d1b8c126d8c50e998fe95f978f2f89e23b
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-08-02 23:05:11 +0530

    Time: 29 ms (99.82%), Space: 16.3 MB (86.04%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,21 +1,13 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        if len(s) == 0:
-            return t
         
-        hashTable = {}
+        ans  = 0
         
         for i in t:
-            hashTable[i] = 1+hashTable.get(i,0)
-        
-        
+            ans ^= ord(i)
+            
         for i in s:
-            if i in hashTable:
-                hashTable[i] -=1
-                
-                if hashTable[i] == 0:
-                    del hashTable[i]
-        
-        for i in hashTable:
-            return i
+            ans ^=ord(i)
+            
+        return chr(ans)
