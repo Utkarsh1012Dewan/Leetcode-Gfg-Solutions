56af1938b0eba6e1256a25d7a416d43cd24c42f3
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-08-25 20:38:36 +0530

    Time: 128 ms (74.80%), Space: 17.7 MB (77.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,7 +4,7 @@ class Solution:
         #bit manipulation s0lution
         
         total = 0
-        
+        #using i+1 because we want to cover all the numbers till n. And also missing 0 in the first iteration does not matter because similar number will give us a zero anyways after getting XORed
         for i,ele in enumerate(nums):
             total ^= (i+1^ele)
             
