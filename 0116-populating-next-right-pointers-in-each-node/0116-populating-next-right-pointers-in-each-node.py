90b902e56b304a668566e8a35d86bd96482616bf
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-09-01 13:27:54 +0530

    Time: 58 ms (85.22%), Space: 18.1 MB (66.72%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -13,15 +13,21 @@ class Solution:
         
         if not root:
             return None
-        queue = [root]
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                node = queue.pop(0)
-                if i < size - 1:
-                    node.next = queue[0]
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        leftmost = root
+         
+        #this loop will update the levels
+        while leftmost.left:
+            head = leftmost
+
+            #this loop will connect the pointers
+            while head:
+                head.left.next = head.right
+
+                #checking whether there is another node on the same level(connecting right Node of first subtree to the left node of the second subtree on the same level)
+                if head.next:
+                    head.right.next = head.next.left
+                #moving to the second node on the same level
+                head = head.next
+            #traversing to other nodes on left side
+            leftmost = leftmost.left
         return root
