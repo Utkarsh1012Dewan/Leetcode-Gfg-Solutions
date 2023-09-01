b8e871864f70f7af34c481058f5bbc465230ff86
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-09-01 13:22:55 +0530

    Time: 53 ms (96.12%) | Memory: 18.1 MB (66.72%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -13,15 +13,17 @@ class Solution:
         
         if not root:
             return None
-        queue = [root]
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                node = queue.pop(0)
-                if i < size - 1:
-                    node.next = queue[0]
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        leftmost = root
+         
+        #this loop will update the levels
+        while leftmost.left:
+            head = leftmost
+
+            #this loop will connect the pointers
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
         return root
