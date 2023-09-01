461d8b299c845f28c0d6eb9ebc0e5fc2b556f82b
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-09-01 16:59:57 +0530

    Time: 30 ms (98.59%) | Memory: 16.4 MB (76.33%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,27 +7,43 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
-        queue = deque()
-        if root:
-            queue.append(root)
+        def dfs(root1,root2):
+
+            if root1 == None and root2 == None:
+                return True
+            
+            if (root1 and root2 == None) or (root1 == None and root2):
+                return False
+            
+            if root1.val == root2.val:
+                return dfs(root1.left,root2.right) and dfs(root1.right,root2.left)
+
+        return dfs(root.left,root.right)
+
+
+
+        # #My BFS approach
+        # queue = deque()
+        # if root:
+        #     queue.append(root)
         
-        while queue:
+        # while queue:
 
-            temp = []
+        #     temp = []
 
-            for i in range(len(queue)):
-                cur = queue.popleft()
-                if cur:
-                    temp.append(cur.val)
-                    queue.append(cur.left)
-                    queue.append(cur.right)
-                else:
-                    temp.append(None)
+        #     for i in range(len(queue)):
+        #         cur = queue.popleft()
+        #         if cur:
+        #             temp.append(cur.val)
+        #             queue.append(cur.left)
+        #             queue.append(cur.right)
+        #         else:
+        #             temp.append(None)
 
-            if len(temp)== 1:
-                continue
-            n = len(temp)
-            for i in range(n//2):
-                if temp[i] != temp[n-i-1]:
-                    return False
-        return True        
+        #     if len(temp)== 1:
+        #         continue
+        #     n = len(temp)
+        #     for i in range(n//2):
+        #         if temp[i] != temp[n-i-1]:
+        #             return False
+        # return True        
