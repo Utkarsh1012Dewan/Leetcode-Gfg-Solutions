9640f58d6f539d5dde7204b90e85aea6e98cb402
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-10-13 19:02:14 +0530

    Time: 72 ms (6.76%), Space: 17.5 MB (17.92%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -3,26 +3,31 @@ class Solution:
 
         n = len(cost)
 
-        dp1 = [-1]*(n)
-        dp2 = [-1]*(n)
+        dp = [-1]*(n)
 
-        ans1 = self.helper(n-1,cost,dp1)
-        ans2 = self.helper(n-2,cost,dp2)
+        ans1 = self.helper(n-1,cost,dp,n)
+
+        dp=[-1]*(n)
+
+        ans2 = self.helper(n-2,cost,dp,n)
 
         return min(ans1,ans2)
 
 
-    def helper(self,index,cost,dp):
+    def helper(self,index,cost,dp,n):
 
         if index < 0:
             return 0
         
+        if n <= 1:
+            return cost[n]
+        
         if dp[index] != -1:
             return dp[index]
 
-        left = self.helper(index-1,cost,dp) + cost[index]
+        left = self.helper(index-1,cost,dp,n) + cost[index]
 
-        right = self.helper(index-2,cost,dp) + cost[index]
+        right = self.helper(index-2,cost,dp,n) + cost[index]
 
         dp[index] = min(left,right)
 
