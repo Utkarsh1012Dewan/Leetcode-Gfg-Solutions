c390174d499cfc47998841c373c7b287f34b6c11
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-11-05 22:03:54 +0530

    Time: 253 ms (47.33%) | Memory: 16.9 MB (17.17%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:      
+
+        minEle = -1
+        count  = -1
+        track = {}
+
+        for i in nums:
+            track[i] = 1 + track.get(i,0)
+
+        for i in track:
+            if i % 2 == 0:
+                if count == -1 and minEle == -1:
+                    minEle = i
+                    count = track[i]
+                elif track[i] == count and i < minEle:
+                    minEle = i
+                elif track[i] > count:
+                    minEle = i
+                    count = track[i]
+        
+        return minEle
