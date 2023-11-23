6a4822c43a4b20cb9e7129187b3d02e232579589
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-11-23 16:24:14 +0530

    Time: 145 ms (99.33%) | Memory: 16.5 MB (39.64%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -1,17 +1,49 @@
 class Solution:
+#MORE OPTIMIZED APPROACH
+    def helper(self,arr):
+        max_ele = max(arr)
+        min_ele = min(arr)
+
+        #this calculates the remainder when the range of the array (i.e., the difference b/w the max and min elements) is divided by the number of gaps between the elements (which is len(arr) - 1).
+        if (max_ele-min_ele) % (len(arr)-1) != 0:
+            return False
+        diff = (max_ele-min_ele) / (len(arr)-1)
+
+        new = set(arr)
+        curr = min_ele + diff
+        while curr < max_ele:
+            if curr not in new:
+                return False
+            curr += diff
+        return True
+
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        ans = [False]*len(l)
-
+        ans = []
         for i in range(len(l)):
-            subarray = sorted(nums[l[i]:r[i]+1])
-            diff = subarray[1] - subarray[0]
-            ans[i] = True
-            for j in range(1, len(subarray)):
-                if subarray[j] - subarray[j-1] != diff:
-                    ans[i] = False
-                    break
-
+            arr = nums[l[i] : r[i] + 1]
+            ans.append(self.helper(arr))
+        
         return ans
+
+    
+
+
+
+
+
+#LESS OPTIMIZED APPROACH
+        # ans = [False]*len(l)
+
+        # for i in range(len(l)):
+        #     subarray = sorted(nums[l[i]:r[i]+1])
+        #     diff = subarray[1] - subarray[0]
+        #     ans[i] = True
+        #     for j in range(1, len(subarray)):
+        #         if subarray[j] - subarray[j-1] != diff:
+        #             ans[i] = False
+        #             break
+
+        # return ans
 
                 
 
