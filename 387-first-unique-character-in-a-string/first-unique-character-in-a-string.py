496ed2c722d49cdae60f193a21ff17bc15db9125
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-02-05 22:13:49 +0530

    Time: 116 ms (40.20%) | Memory: 16.7 MB (78.97%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,19 +1,48 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
-        track = {}
-        
-        for i in s:
-            track[i] = 1+track.get(i,0)
-        
-        check = 0
-        
-        for i in track:
-            if track[i] == 1:
-                check =  i
-                break
+
+        arr = [0]*26
         for i in range(len(s)):
-            if s[i] == check:
+            arr[ord(s[i])-ord('a')] +=1
+        
+        for i in range(len(s)):
+            if arr[ord(s[i])-ord('a')] == 1:
                 return i
         
         return -1
+
+
+
+
+#better solution
+
+# class Solution:
+#     def firstUniqChar(self, s: str) -> int:
+#         arr = [0]*26
+#         for i in range(len(s)):
+#             arr[ord(s[i]) - ord('a')] += 1
+        
+#         for i in range(len(s)):
+#             if arr[ord(s[i]) - ord('a')] == 1:
+#                 return i
+
+#         return -1
+# class Solution:
+#     def firstUniqChar(self, s: str) -> int:
+        
+#         track = {}
+        
+#         for i in s:
+#             track[i] = 1+track.get(i,0)
+        
+#         check = 0
+        
+#         for i in track:
+#             if track[i] == 1:
+#                 check =  i
+#                 break
+#         for i in range(len(s)):
+#             if s[i] == check:
+#                 return i
+        
+#         return -1
