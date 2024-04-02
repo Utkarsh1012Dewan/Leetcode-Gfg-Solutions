891473c0cd29e5b7212d83f25d0e03d29fa267a9
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-04-02 13:25:03 +0530

    Time: 35 ms (98.01%) | Memory: 18.1 MB (19.56%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,17 +1,16 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
 
-        if int(num[-1]) % 2 != 0:
+        n = len(num)
+
+        odds = set('13579')
+        if num[-1] in odds:
             return num
 
-        result = 0
-
         for i in range(len(num)-1,-1,-1):
-            if num[i] == 0:
-                continue
-            if int(num[i]) % 2 != 0:
-                result = num[:i+1]
-                break
-
-        return "" if result == 0 else str(result)
+            if num[i] in odds and i < n-1:
+                return num[:i+1]
+                
+        
+        return ""
         
