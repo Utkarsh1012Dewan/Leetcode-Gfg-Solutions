500d3337d104d7d75d686a75a8e04cc4ddcefd73
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-04 11:23:59 +0530

    Time: 37 ms (54.97%), Space: 16.7 MB (34.5%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+
+        track = set()
+        res = 0
+
+        for i in s:
+            if i in track:
+                track.remove(i)
+                res +=2
+            else:
+                track.add(i)
+        
+        return res + 1 if track else res
+
+
+
+
+
+#Wrong code
+        # if len(s) == 1:
+        #     return 1
+        # track = defaultdict(int)
+
+        # for i in s:
+        #     track[i] +=1
+        # isEven,isOdd = 0,0
+        # evenTotal,oddTotal = 0,0
+        # oddMaxFreq = 0
+
+        # for i in track:
+        #     if track[i]  %2 == 0:
+        #         isEven +=1
+        #         evenTotal += track[i]
+        #     else:
+        #         isOdd +=1
+        #         oddTotal += track[i]
+        #         oddMaxFreq = max(oddMaxFreq,track[i])
+                    
+        # if len(s) % 2 == 0 and isOdd == 0:
+        #     return evenTotal
+        # elif len(2) % 2 == 0 and isOdd > 0:
+        #     evenTotal + oddMaxFreq 
+
+        # elif isOdd % 2 == 0:
+        #     return evenTotal + oddMaxFreq
+        # else:
+        #     return evenTotal + oddTotal
+        
+
+        
