cf184931a35e8ee5c9120a57cb377067a3e6bd3c
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-10 10:29:56 +0530

    Time: 44 ms (24.96%), Space: 16.4 MB (77.64%) - LeetHub

--- 1137-height-checker/1137-height-checker.py
+++ 1137-height-checker/1137-height-checker.py
@@ -1,12 +1,30 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        
-        arr = sorted(heights)
-        count = 0
+        res = [0]*101
 
-        for i in range(len(heights)):
-            if heights[i] != arr[i]:
-                count +=1
+        for i in heights:
+            res[i] +=1
         
-        return count
+        expected = []
+        for i in range(1,101):
+            #this c will tell how many times the number is occuring in the list
+            c = res[i]
+            for _ in range(c):
+                expected.append(i)
+        
+        res = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res +=1
+        return res
+
+# #Brute Force 0 O(nlogn)
+#         arr = sorted(heights)
+#         count = 0
+
+#         for i in range(len(heights)):
+#             if heights[i] != arr[i]:
+#                 count +=1
+        
+#         return count
         
