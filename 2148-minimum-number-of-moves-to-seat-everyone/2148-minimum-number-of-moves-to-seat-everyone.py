4c65adf30dbf5c3ebbfcc79d067dce6658a544e9
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-13 10:11:08 +0530

    Time: 65 ms (21.37%), Space: 16.5 MB (82.54%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
@@ -1,13 +1,40 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        #Counting Sort
+        maxInd = max(max(seats),max(students))+1
+        countSeats = [0] * maxInd
+        countStudents = [0] * maxInd
 
-        seats.sort()
-        students.sort()
-        #[1,3,5]
-        #[2,4,7]
-        count = 0
-
-        for i in range(len(seats)):
-            count += abs(students[i]-seats[i])
+        for seat in seats:
+            countSeats[seat] +=1
+        for student in students:
+            countStudents[student] +=1
         
-        return count
+        i,j = 0,0
+        remaining = len(students)
+        res = 0
+
+        while remaining:
+            if countSeats[i] == 0:
+                i+=1
+            if countStudents[j] == 0:
+                j+=1
+            if countSeats[i] and countStudents[j]:
+                res += abs(i-j)
+                countSeats[i]-=1
+                countStudents[j]-=1
+                remaining -=1
+        return res
+
+        # #Sorting algo - O(NLogN)
+
+        # seats.sort()
+        # students.sort()
+        # #[1,3,5]
+        # #[2,4,7]
+        # count = 0
+
+        # for i in range(len(seats)):
+        #     count += abs(students[i]-seats[i])
+        
+        # return count
