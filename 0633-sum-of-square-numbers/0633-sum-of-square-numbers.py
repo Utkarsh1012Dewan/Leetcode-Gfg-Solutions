217013a6164337422d896002df5b4769378100bc
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-17 10:40:51 +0530

    Time: 951 ms (5%), Space: 16.6 MB (39.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,38 +1,34 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
+        #Binary Search
+        l,r = 0,int(sqrt(c)) + 1
+        for num in range(r+1):
+            l,h = num,r
+            while l<=h:
+                mid = (l+h)//2
+                power = (num**2) + (mid**2)
 
-        # l,r = 0,c
+                if power > c:
+                    h = mid-1
+                elif power < c:
+                    l = mid + 1
+                else:
+                    return True
+
+        return False 
+
+
+        # #Two Pointer -> O(sqrt(c))
+        # l,r = 0,int(sqrt(c))
 
         # while l <= r:
-        #     mid = (l+r)//2
-
-        #     if l*l + (mid*mid) == c or r*r + (mid*mid) == c:
+        #     if (l*l) + (r*r) == c:
         #         return True
-        #     elif l*l + (mid*mid) > c:
-        #         r = mid -1
-        #     else
-        #         l = mid + 1
+        #     elif (l*l) + (r*r) > c:
+        #         r-=1
+        #     elif (l*l) + (r*r) < c:
+        #         l+=1
         
         # return False
-
-#l - 0 
-#r - 5
-#mid - 2
-#4
-
-
-
-# #Two Pointer -> Gives TLE
-        l,r = 0,int(sqrt(c))
-
-        while l <= r:
-            if (l*l) + (r*r) == c:
-                return True
-            elif (l*l) + (r*r) > c:
-                r-=1
-            elif (l*l) + (r*r) < c:
-                l+=1
-        
-        return False
 
 
