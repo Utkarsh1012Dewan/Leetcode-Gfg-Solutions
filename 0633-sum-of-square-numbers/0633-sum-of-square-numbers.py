31b7c261687a6e8b4ef444f611ff6f4e05a9ff7a
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-17 10:41:11 +0530

    Time: 978 ms (5%), Space: 16.4 MB (79.87%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -18,17 +18,17 @@ class Solution:
         return False 
 
 
-        # #Two Pointer -> O(sqrt(c))
-        # l,r = 0,int(sqrt(c))
+        #Two Pointer -> O(sqrt(c))
+        l,r = 0,int(sqrt(c))
 
-        # while l <= r:
-        #     if (l*l) + (r*r) == c:
-        #         return True
-        #     elif (l*l) + (r*r) > c:
-        #         r-=1
-        #     elif (l*l) + (r*r) < c:
-        #         l+=1
+        while l <= r:
+            if (l*l) + (r*r) == c:
+                return True
+            elif (l*l) + (r*r) > c:
+                r-=1
+            elif (l*l) + (r*r) < c:
+                l+=1
         
-        # return False
+        return False
 
 
