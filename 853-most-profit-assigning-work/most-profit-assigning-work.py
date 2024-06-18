33a334fa6138be86b416ad5ec13693a0c459b20f
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-18 11:06:31 +0530

    Time: 531 ms (8.00%) | Memory: 19.6 MB (56.95%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,22 +1,25 @@
 class Solution(object):
     def maxProfitAssignment(self, difficulty, profit, worker):
+        # Find the maximum difficulty to create an array up to this difficulty
         max_difficulty = max(difficulty)
         max_Upto_Difficulty = [0] * (max_difficulty + 1)
 
-        #mapping each difficulty to its profit
+        # Map each difficulty to its highest profit
         for d, p in zip(difficulty, profit):
             max_Upto_Difficulty[d] = max(max_Upto_Difficulty[d], p)
 
-        #keeping a track of the maxProfit upto a difficulty
+        # Update the array so each difficulty has the maximum profit up to that difficulty
         for i in range(1, max_difficulty + 1):
             max_Upto_Difficulty[i] = max(max_Upto_Difficulty[i], max_Upto_Difficulty[i - 1])
 
-        profit = 0
+        # Calculate total profit by adding the maximum profit for each worker's ability
+        total_profit = 0
         for ability in worker:
+            # If worker's ability exceeds the maximum difficulty, add the highest possible profit
             if ability > max_difficulty:
-                profit += max_Upto_Difficulty[max_difficulty]
+                total_profit += max_Upto_Difficulty[max_difficulty]
             else:
-                profit += max_Upto_Difficulty[ability]
+                # Otherwise, add the maximum profit for the worker's ability
+                total_profit += max_Upto_Difficulty[ability]
 
-        return profit
-        
+        return total_profit
