6dbdef22f27e73660e0beee6c34194d4d6186623
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-20 10:26:12 +0530

    Time: 894 ms (60.4%), Space: 30.5 MB (25.64%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+
+        n = len(position)
+        position.sort()
+
+        def count(dist):
+            #ans = 1 because we have atleast one ball placed at the front
+            ans,curr = 1,position[0]
+            for i in range(1,n):
+                #if the distance between the first ball -> at position[0]
+                #is >= d, that means we can try and put a ball there
+                #we keep doing this to find how many balls we can place at any given distance
+                if position[i] - curr >= dist:
+                    ans +=1
+                    curr = position[i]
+            return ans
+
+
+        l,r = 0, position[-1] - position[0]
+        while l < r:
+            mid = r - (r-l) // 2
+
+            #if the balls places in at "mid" sitance from each other is hgher than m
+            #then we can increase the distance we can put between the balls
+            if count(mid) >= m:
+                l = mid
+            #if the count of balls if lower than m
+            #that means the distance between balls is too high and
+            #we need to decrease that distance beteween the balls
+            else:
+                r = mid - 1
+        
+        return l
+        
