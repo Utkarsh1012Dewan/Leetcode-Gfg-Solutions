d445c15900debfa2109738efb4d02c9c443987d4
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-25 10:44:35 +0530

    Time: 40 ms (26.92%), Space: 16.4 MB (74.62%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
@@ -5,12 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    #to keep a global sum of all the nodes
     val = 0
     def bstToGst(self, root: TreeNode) -> TreeNode:
       if root:
         self.bstToGst(root.right)
+
+        #1. we make the value of root = the sum of value of all the nodes
+        #   that have a key valuie greater than itself
+
+        #2.We also update the global sum with the new value of the root
+        #   for other upcoming nodes
         root.val = self.val = self.val + root.val
+
+        
         self.bstToGst(root.left)
+        
         return root
         
 
