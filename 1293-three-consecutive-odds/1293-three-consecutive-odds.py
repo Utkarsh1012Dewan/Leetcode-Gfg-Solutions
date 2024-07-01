6fac8a59b29af32e62c7bd86167ac7703d294288
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-01 09:27:24 +0530

    Time: 53 ms (18.2%), Space: 16.5 MB (93.64%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.py
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.py
@@ -1,8 +1,7 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-
+#My Method
         count = 0
-
         for i in arr:
             if i % 2 != 0:
                 count +=1
@@ -10,6 +9,12 @@ class Solution:
                     return True
             else:
                 count = 0
-        
         return False
         
+
+#Another clever way I looked from solutions
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+            for i in range(1, len(arr) - 1):
+                if arr[i - 1] % 2 + arr[i] % 2 + arr[i + 1] % 2 == 3:
+                    return True
+            return False
