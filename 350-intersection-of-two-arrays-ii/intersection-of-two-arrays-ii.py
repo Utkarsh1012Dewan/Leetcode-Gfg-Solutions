b3f4bbc868c0dc7cd2427ff38ae3f9170a490a98
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-02 09:22:49 +0530

    Time: 46 ms (71.32%) | Memory: 16.7 MB (31.35%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,15 +1,34 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-        track = {}
-        for i in nums1:
-            track[i] = 1 + track.get(i,0)
-        
+        #Approach 2 - After sorting
+        nums1.sort()
+        nums2.sort()
+        i,j = 0,0
         ans = []
-
-        for i in nums2:
-            if i in track and track[i] > 0:
-                ans.append(i)
-                track[i] -=1
-        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i+=1
+            elif nums1[i] > nums2[j]:
+                j+=1
+            else:
+                ans.append(nums1[i])
+                i+=1
+                j+=1
         return ans
+
+
+# TC - O(M+N)
+# SC - O(min(M,N)) where K is the len of either nums1 or nums2
+        # track = {}
+        # for i in nums1:
+        #     track[i] = 1 + track.get(i,0)
+        
+        # ans = []
+
+        # for i in nums2:
+        #     if i in track and track[i] > 0:
+        #         ans.append(i)
+        #         track[i] -=1
+        
+        # return ans
