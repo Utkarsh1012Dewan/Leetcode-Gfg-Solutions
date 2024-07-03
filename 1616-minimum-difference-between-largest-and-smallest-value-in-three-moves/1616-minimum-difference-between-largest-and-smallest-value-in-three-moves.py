71a12349998dfeb87e6192e6c009ac05cd8c9d08
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-03 10:05:23 +0530

    Time: 239 ms (99.06%), Space: 27.5 MB (26.89%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,10 +1,29 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
-
-        nums.sort()
+        
+        #This will stay there for both the solutions
+        #To handle an important edge  case
         if len(nums) <= 4:
             return 0
         res = float("inf")
+
+        #T.C= O(N) -> Using Heap (Extremely clever  solution)
+        #Idea->We are going to use heap to keep track of
+        #4 smallest and 4 largest elements in the array
+        #take  min of smallest[i]-  largest[i], both of which the arrays will be sorted
+        res = float("inf")
+        small = sorted(heapq.nsmallest(4,nums))
+        large = sorted(heapq.nlargest(4,nums))
+
+        for i in range(4):
+            res = min(res,large[i]-small[i])
+        
+        return res
+
+
+        #TC - O(NLogN)
+        #https://www.youtube.com/watch?v=S6cUjbQuTnE
+        nums.sort()
 
         for l in range(4):
             r = len(nums)-4+l
