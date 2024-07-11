b32ef2ddb2314e64af0ebeb4701667de38cfbb82
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-12 00:54:15 +0530

    Time: 48 ms (71.86%) | Memory: 49 MB (10.85%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,9 +1,9 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
-    return () => 'Hello World'
-};
+function createHelloWorld(){
+    return ()=> "Hello World"
+}
 
 /**
  * const f = createHelloWorld();
