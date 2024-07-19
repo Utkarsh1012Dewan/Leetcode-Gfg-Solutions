653f0b6096bc7ffd58e8c86738def47a4e70540d
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-19 11:58:53 +0530

    Time: 107 ms (73.7%), Space: 16.9 MB (43.01%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,39 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        rows,cols = len(matrix),len(matrix[0])
+        min_row, max_col = {},{}
+
+        #getting the max element from every row
+        for i in range(rows):
+            rmin = float("inf")
+            for j in range(cols):
+                if matrix[i][j] < rmin:
+                    rmin = min(matrix[i][j],rmin)
+            min_row[rmin] = i
+
+        #getting the max element from every column
+        for j in range(cols):
+            cmax = 0
+            for i in range(rows):
+                if matrix[i][j] > cmax:
+                    cmax = max(matrix[i][j],cmax)
+            max_col[cmax] = j
+
+        ans = []
+
+        for i in min_row:
+            if i in max_col:
+                ans.append(i)
+        
+
+        return ans
+
+        
+
+                
+            
+
+
+
+
+        
