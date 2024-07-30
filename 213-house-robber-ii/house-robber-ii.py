3427347c1bc19f66e58ca329f5854eb7ce470ff1
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-07-30 11:03:53 +0530

    Time: 38 ms (44.88%) | Memory: 16.6 MB (26.33%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -24,12 +24,11 @@ class Solution:
         
         dp1 = [-1]*(n+1)
         dp2 = [-1]*(n+1)
+        # arr1 = nums[1:]
+        # arr2 = nums[0:n-1]
 
-        arr1 = nums[1:]
-        arr2 = nums[0:n-1]
-
-        ans1 = helper(n-2,arr1,dp1)
-        ans2 = helper(n-2,arr2,dp2)
+        ans1 = helper(n-2,nums[1:],dp1)
+        ans2 = helper(n-2,nums[0:n-1],dp2)
 
         return max(ans1,ans2)
 
