23fb10406b8c18606acae10bbbf2988eaee1d2b6
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-08-15 13:39:55 +0530

    Time: 609 ms (85.71%), Space: 21.2 MB (15.58%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,25 +1,49 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         fives,tens = 0,0
-
+#more clean code
         for i in bills:
             if i == 5:
                 fives +=1
-            elif i == 10:
-                if fives == 0:
-                    return False
-                else:
+            
+            if i == 10:
+                if fives > 0:
                     tens +=1
                     fives -=1
-            elif i == 20:
-                if fives == 0:
+                else:
                     return False
-                elif tens == 0: 
-                    if fives < 3:
-                        return False
-                    else:
-                        fives -= 3
-                elif tens > 0 and fives > 0:
+            
+            if i == 20:
+                if tens > 0 and fives > 0:
                     tens -=1
                     fives -=1
+                elif fives >= 3:
+                    fives -=3
+                else:
+                    return False
         return True
+
+
+
+# #my code (badly written)
+#         for i in bills:
+#             if i == 5:
+#                 fives +=1
+#             elif i == 10:
+#                 if fives == 0:
+#                     return False
+#                 else:
+#                     tens +=1
+#                     fives -=1
+#             elif i == 20:
+#                 if fives == 0:
+#                     return False
+#                 elif tens == 0: 
+#                     if fives < 3:
+#                         return False
+#                     else:
+#                         fives -= 3
+#                 elif tens > 0 and fives > 0:
+#                     tens -=1
+#                     fives -=1
+#         return True
